0e289598931b8d959210c035a6466e55d3410f04
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-22 14:38:24 -0800

    shuffleDeck

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { v4 as uuid} from "uuid";
 import Card from "./Card";
-const DECKIDURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
+const DECK_ID_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
 /*
  * Props: none
  * States:
@@ -19,11 +19,12 @@ function GameBoard() {
   const [cardsOnBoard, setCardsOnBoard] = useState([]);
   const [deckId, setDeckId] = useState("");
   const [drawingCard, setDrawingCard] = useState(false);
+  const [shufflingDeck, setShufflingDeck] = useState(false);
 
   /* set deckid on mount */
   useEffect(function getDeckIdOnMount() {
     async function getDeckId() {
-      const resp = await axios.get(DECKIDURL);
+      const resp = await axios.get(DECK_ID_URL);
       setDeckId(id => resp.data.deck_id);
     }
     getDeckId();
@@ -35,15 +36,36 @@ function GameBoard() {
    */
   useEffect(function putCardOnBoard() {
     async function getCard() {
-      if(drawingCard){
-        const DRAWCARDURL = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
-        const resp = await axios.get(DRAWCARDURL);
-        setCardsOnBoard(cards => [...cards, {...resp.data.cards[0], key: uuid()} ]);
-      }
+      const drawCardURL = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+      const resp = await axios.get(drawCardURL);
+      setCardsOnBoard(cards => [...cards, {...resp.data.cards[0], key: uuid()} ]);
     }
-    getCard();
+    if(drawingCard) getCard();
     setDrawingCard(false);
-  }, [drawingCard, deckId])
+  }, [drawingCard, deckId]);
+
+  /* draw a new card and put card on board
+   * Gets card from DRAWCARDURL
+   * adds card to state: "cardsOnBoard"
+   */
+  useEffect(function shuffleDeck() {
+    async function getShuffledDeck() {
+      const shuffleDeckURL = `https://deckofcardsapi.com/api/deck/${deckId}/shuffle`;
+      await axios.get(shuffleDeckURL);
+      setCardsOnBoard([]);
+      setShufflingDeck(false);
+    }
+    if (shufflingDeck) getShuffledDeck();
+  }, [shufflingDeck, deckId]);
+
+  /**
+   * Handles the click of "Shuffle Deck" button
+   * sets shufflingdeck = true
+   * disables "shuffle deck" button event handler
+   */
+  function handleShuffle(evt){
+    setShufflingDeck(true);
+  }
 
   /**
    * handle drawing card button click
@@ -57,6 +79,7 @@ function GameBoard() {
 
   return (<div className="GameBoard">
      <button className="GameBoard-draw-button" onClick={handleDrawingCard}>Gimme a Card!</button>
+     <button className="GameBoard-shuffle-button" onClick={(shufflingDeck)? null : handleShuffle}>Shuffle Deck</button>
     <div>
       {cardsOnBoard.map(c => <Card image={c.image} code={c.code} key={c.key}/>)}
     </div>
